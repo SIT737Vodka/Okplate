7fcd31325666735a32c0c33bedead3989804fc9b
ansleydeakin <38422697+ansleydeakin@users.noreply.github.com>
2018-04-27 00:53:54 +1000

    Add files via upload
    web-server.js - Main web server ("node web-server.js" to run)
    test.js - allows testing of puppeteer and scrap without image recognition ("node test.js" to run)

---
+++ test.js
@@ -0,0 +1,35 @@
+const puppeteer = require('puppeteer');
+
+     // Puppeteer for automation and scrapping of data from Vicroads 
+     let scrape = async () => {
+        const browser = await puppeteer.launch({headless: true});
+        const page = await browser.newPage();
+    
+        await page.goto('https://www.vicroads.vic.gov.au/registration/buy-sell-or-transfer-a-vehicle/buy-a-vehicle/check-vehicle-registration/vehicle-registration-enquiry?utm_source=VR-checkrego&utm_medium=button&utm_campaign=VR-checkrego');
+        await page.type("input#ph_pagebody_0_phthreecolumnmaincontent_1_panel_VehicleSearch_RegistrationNumberCar_RegistrationNumber_CtrlHolderDivShown",'AOI190');
+       // await page.waitFor(10000);
+        await page.click("input#ph_pagebody_0_phthreecolumnmaincontent_1_panel_btnSearch"); // Click the search button to submit
+        await page.waitFor(1000);
+    
+        const result = await page.evaluate(() => {
+            let data = []; // Create an empty array that will store our data
+            let elements = document.querySelectorAll('.detail-module-content'); 
+    
+            for (var element of elements){ // Loop through each proudct
+                let Status = element.childNodes[3].innerText; // Select the Status and Expiry Date
+                let Vehicle = element.childNodes[5].innerText; // Select the Vehicle
+                let VIN = element.childNodes[7].innerText; // Select the VIN
+    
+                data.push({Status,Vehicle,VIN}); // Push an object with the data onto our array
+            }
+    
+            return data; // Return our data array
+        });
+    
+        browser.close();
+        return result; // Return the data
+    };
+
+    scrape().then((value) => {
+          console.log(value); // Success!
+});
--- web-server.js
+++ web-server.js
@@ -4,6 +4,13 @@ const multipart = require('connect-multiparty');
 const cloudinary = require('cloudinary');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const puppeteer = require('puppeteer');
+
+//variables required for cheerio 
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+
 
 // we set the port programmatically, in case we need to change it later
 var port = 3000;
@@ -20,25 +27,66 @@ app.use(cors());
 
 const multipartMiddleware = multipart();
 
-//Cloudinary & OCR Add-on powered by Googlevision API
+//Cloudinary & OCR Add-on powered by Googlevision API (Need to make a new account before submitting this)
 cloudinary.config({
-    cloud_name: 'dr9pssgmx',
-    api_key: '273599616817251',
-    api_secret: 'fJA0eun_Z6r7zdCKbOgmzFc1z1k'
+    cloud_name: 'dgque5s6r',
+    api_key: '619334687283924',
+    api_secret: '6HQ8h1PCHPf6TEyI33dUJ9q-mIw'
 });
 
 app.post('/upload', multipartMiddleware, function(req, res) {
-    cloudinary.v2.uploader.upload(req.files.image.path,
-      {
-        ocr: "adv_ocr"
+  cloudinary.v2.uploader.upload(req.files.image.path,
+    {
+      ocr: "adv_ocr"
       }, function(error, result) {
           if( result.info.ocr.adv_ocr.status === "complete" ) {
-            res.json(result); // result.info.ocr.adv_ocr.data[0].textAnnotations[0].description (more specific)
-          }
+       
+        // API returns rego in JSON 
+      let regoraw = `${result.info.ocr.adv_ocr.data[0].textAnnotations[0].description}`;
+          console.log(regoraw);
+          rego = regoraw.replace(/\s/g, ''); //remove spaces from string to bypass vicroad validation
+          console.log(rego);
+          
+          // Puppeteer for automation and scrapping of data from Vicroads 
+          let scrape = async () => {
+            const browser = await puppeteer.launch({headless: true});
+            const page = await browser.newPage();
+        
+            await page.goto('https://www.vicroads.vic.gov.au/registration/buy-sell-or-transfer-a-vehicle/buy-a-vehicle/check-vehicle-registration/vehicle-registration-enquiry?utm_source=VR-checkrego&utm_medium=button&utm_campaign=VR-checkrego');
+            await page.type("input#ph_pagebody_0_phthreecolumnmaincontent_1_panel_VehicleSearch_RegistrationNumberCar_RegistrationNumber_CtrlHolderDivShown",rego);
+           // await page.waitFor(10000);
+            await page.click("input#ph_pagebody_0_phthreecolumnmaincontent_1_panel_btnSearch"); // Click the search button to submit
+            await page.waitFor(1000);
+        
+            const result = await page.evaluate(() => {
+                let data = []; // Create an empty array that will store our data
+                let elements = document.querySelectorAll('.detail-module-content'); 
+        
+                for (var element of elements){ // Loop through each proudct
+                    let Status = element.childNodes[3].innerText; // Select the Status and Expiry Date
+                    let Vehicle = element.childNodes[5].innerText; // Select the Vehicle
+                    let VIN = element.childNodes[7].innerText; // Select the VIN
+        
+                    data.push({Status,Vehicle,VIN}); // Push an object with the data onto our array
+                }
+        
+                return data; // Return our data array
+            });
+        
+            browser.close();
+            return result; // Return the data
+        };
+        
+        scrape().then((value) => {
+          let Registration = `${Status.info.ocr.adv_ocr.data[0].textAnnotations[0].description}`;
+            console.log(value); // Success!
+        });
+        }
+        
       });
   });
-
-
+  
+  
   
   //start the app and listen to the port
 
